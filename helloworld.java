21eceaa5e854adc17b40cbb1929b91833b469ad9
Sagar Ghorad <114870075+shaggyweb3@users.noreply.github.com>
2023-01-13 20:34:28 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! its me sagar");
+        System.out.println("Hello, World! ");
     }
 }
